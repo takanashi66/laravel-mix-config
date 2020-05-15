ecbf03bc93d974ac9f331667497c9806fcbd3964
takanashi66 <t.inoue@funaffect.jp>
2020-05-16 01:16:39 +0900

    laravel-mix4に合わせて他もアップデート

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,30 +1,36 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/assets/js/script.js', 'public/common/js')
-   .sass('resources/assets/scss/style.scss', 'public/common/css')
-   .options({
-      processCssUrls: false,
-      postCss: [
-        require('css-mqpacker')()
-      ],
-      autoprefixer: {
-        browsers: ['last 2 versions'],
-        options: {
-          grid: true,
+    .sass('resources/assets/scss/style.scss', 'public/common/css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            require('css-mqpacker')()
+        ],
+        autoprefixer: {
+            browsers: ['last 2 versions'],
+            options: {
+                grid: true,
+            }
         }
-      }
-   })
-   .sourceMaps()
-   
-mix.webpackConfig({ devtool: "inline-source-map" })
+    })
+    .sourceMaps()
+
+mix.webpackConfig({
+    devtool: "inline-source-map"
+})
 
 mix.browserSync({
-  server: 'public',
-  proxy: false,
-  files: [
-    'public/**/*.html',
-    'public/**/*.php',
-    'public/common/js/*.js',
-    'public/common/css/*.css',
-  ]
+    server: 'public',
+    proxy: false,
+    port: 3000,
+    files: [
+        'public/**/*.html',
+        'public/**/*.php',
+        'public/common/js/*.js',
+        'public/common/css/*.css',
+    ],
+    ui: {
+        port: 3001
+    }
 })
